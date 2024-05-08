e2e3c7a55efd0deabf8a54641fa9c731f8bf303d
JeseGV <65308030+JeseGV@users.noreply.github.com>
2024-05-08 08:03:00 -0600

    Update people.js

--- DB/people.js
+++ DB/people.js
@@ -1,7 +1,7 @@
 const people = {
     p1: {
         name: "Marcos de Llantas Leomh",
-        img: "/assets/BD/png/Desponchado-yLlantas-Leomh-en-Delicias.jpg"
+        img: "/assets/BD/png/Desponchado-y-Llantas-Leomh-en-Delicias.jpg"
     },
     p2: {
         name: "Manuel de Desponchado 24hrs",
